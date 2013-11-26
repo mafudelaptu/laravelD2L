cc57bdee0cacb45b5a65c3281ab82dae37d23c55
Artur Leinweber <mafudelaptu@gmail.com>
2013-11-26 15:59:06 +0100

    general modal

---
+++ app/views/generalModal.blade.php
@@ -0,0 +1,12 @@
+<div id="generalModal" class="modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        {{-- wird von js gefüllt --}}
+      </div>
+      <div class="modal-footer">
+        {{-- wird von js gefüllt --}}
+      </div>
+    </div>
+  </div>
+</div>
